0156bb14c23c41340eec3d6d6ac801a15977c567
Eric Benedetto <ericsplace3@gmail.com>
2020-08-20 16:20:14 -0400

    fix session on rerender bug

--- app/client/src/components/Chat/index.js
+++ app/client/src/components/Chat/index.js
@@ -8,62 +8,59 @@ import { GlobalContext } from '../../context/GlobalState';
 import { getSession, sendMessage } from '../../api/queries';
 
 const useStyles = makeStyles({
-  wrapper: {
-    margin: '0 auto',
-  },
+    wrapper: {
+        margin: '0 auto',
+    },
 });
 
 export const Chat = () => {
-  const classes = useStyles();
-  const {
-    user_message,
-    message_queue,
-    enqueueMessage,
-    setMessage,
-  } = useContext(GlobalContext);
+    const classes = useStyles();
+    const {
+        user_message,
+        message_queue,
+        enqueueMessage,
+        setMessage,
+    } = useContext(GlobalContext);
 
-  // FIXME: Add Session Management Elsewhere, This is Hacky
-  useEffect(() => {
-    sessionStorage.clear();
-  });
+    useEffect(() => {
+        document
+            .querySelector('#bottom')
+            .scrollIntoView({ behavior: 'smooth' });
+    }, [message_queue]);
 
-  useEffect(() => {
-    document.querySelector('#bottom').scrollIntoView({ behavior: 'smooth' });
-  }, [message_queue]);
-
-  return (
-    <Grid container item xs={12} sm={8} md={6} className={classes.wrapper}>
-      <ChatHeader />
-      <Grid container component={Paper} style={{ height: '100%' }}>
-        <Grid item xs={12}>
-          <List style={{ height: '80vh', overflow: 'auto' }}>
-            {message_queue.map((msg, it) => (
-              <ChatMessage
-                name={msg.name}
-                text={msg.text}
-                time={it.toString()}
-                key={it}
-                // Alt Must Come from Client Side
-                alt={msg.name !== 'User'}
-              />
-            ))}
-            <div id='bottom' />
-          </List>
-          <Divider />
+    return (
+        <Grid container item xs={12} sm={8} md={6} className={classes.wrapper}>
+            <ChatHeader />
+            <Grid container component={Paper} style={{ height: '100%' }}>
+                <Grid item xs={12}>
+                    <List style={{ height: '80vh', overflow: 'auto' }}>
+                        {message_queue.map((msg, it) => (
+                            <ChatMessage
+                                name={msg.name}
+                                text={msg.text}
+                                time={it.toString()}
+                                key={it}
+                                // Alt Must Come from Client Side
+                                alt={msg.name !== 'User'}
+                            />
+                        ))}
+                        <div id='bottom' />
+                    </List>
+                    <Divider />
+                </Grid>
+                <MessageInput
+                    onChange={(e) => {
+                        setMessage(e.target.value);
+                    }}
+                    onSubmit={async () => {
+                        enqueueMessage({ text: user_message, name: 'User' });
+                        const msgs = await sendMessage({ user_message });
+                        msgs.map(({ text }) => {
+                            enqueueMessage({ text, name: 'CUNY Assistant' });
+                        });
+                    }}
+                />
+            </Grid>
         </Grid>
-        <MessageInput
-          onChange={(e) => {
-            setMessage(e.target.value);
-          }}
-          onSubmit={async () => {
-            enqueueMessage({ text: user_message, name: 'User' });
-            const msgs = await sendMessage({ user_message });
-            msgs.map(({ text }) => {
-              enqueueMessage({ text, name: 'CUNY Assistant' });
-            });
-          }}
-        />
-      </Grid>
-    </Grid>
-  );
+    );
 };
