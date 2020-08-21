714145d370c93443ebe0b11aa1c3c80d268c144a
Eric Benedetto <ericbenedetto@Erics-Air.fios-router.home>
2020-08-21 02:23:23 -0400

    rename for clarity

--- app/server/config/ibm-cloud.js
+++ app/server/config/ibm-cloud.js
@@ -2,8 +2,8 @@ const AssistantV2 = require('ibm-watson/assistant/v2');
 const { IamAuthenticator } = require('ibm-watson/auth');
 require('colors');
 
-const { WATSON_API_ROUTE, WATSON_AUTH_KEY } = process.env;
-if (!WATSON_API_ROUTE || !WATSON_AUTH_KEY) {
+const { WATSON_API_ROUTE, WATSON_API_KEY } = process.env;
+if (!WATSON_API_ROUTE || !WATSON_API_KEY) {
   console.log(
     `Error: Missing API Route or Key. Check Your Environment Variable Configuration`
       .red.bold
@@ -12,7 +12,7 @@ if (!WATSON_API_ROUTE || !WATSON_AUTH_KEY) {
 }
 
 const authenticator = new IamAuthenticator({
-  apikey: WATSON_AUTH_KEY,
+  apikey: WATSON_API_KEY,
 });
 
 const assistant = new AssistantV2({
